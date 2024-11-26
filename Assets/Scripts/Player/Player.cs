dfde15800d2e371eb44dfdb52d0a30cc62df7f28
CalebSReed <csereed@gmail.com>
2024-11-25 23:53:27 -0800

    Player slope movement added
    Can move down slope without bouncing now

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,9 +7,13 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _playerSpeed;
     [SerializeField] private Camera _mainCam;
+    [SerializeField] private Rigidbody _rb;
     [SerializeField] private float _turnAroundSpeed;
+    [SerializeField] private float _aimingSpeed;
     private Vector2 _movement;
     private PlayerInput _playerInput;
+    private bool _isSucking;
+    private RaycastHit _slopeHit;
 
     private void Awake()
     {
@@ -32,6 +36,15 @@ public class Player : MonoBehaviour
     {
         LookTowardsMouse();
         DoMovement();
+        if (_playerInput.Player.Fire.ReadValue<float>() > 0)
+        {
+            Suck();
+        }
+    }
+
+    private void Suck()
+    {
+        _isSucking = true;
     }
 
     private void ReadMovement()
@@ -41,7 +54,9 @@ public class Player : MonoBehaviour
 
     private void DoMovement()
     {
-        transform.position += _playerSpeed * Time.fixedDeltaTime * new Vector3(_movement.x, 0, _movement.y);
+        var newDir = new Vector3(_movement.x, 0, _movement.y);
+        newDir = AdjustVelocityToSlope(newDir);
+        _rb.MovePosition(transform.position + _playerSpeed * Time.fixedDeltaTime * newDir);
     }
 
     private void LookTowardsMouse()
@@ -57,7 +72,9 @@ public class Player : MonoBehaviour
         Physics.Raycast(ray, out hit);
         Vector3 lookAtPoint = hit.point;
         lookAtPoint.y = transform.position.y;
-        transform.LookAt(lookAtPoint); 
+        lookAtPoint -= transform.position;
+        lookAtPoint.Normalize();
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(lookAtPoint), _aimingSpeed);
     }
 
     private Vector2 ReadMousePosition()
@@ -72,6 +89,37 @@ public class Player : MonoBehaviour
             Vector3 lookAtRotation = new Vector3(_movement.x, 0, _movement.y);
             Quaternion newRot = Quaternion.LookRotation(lookAtRotation, Vector3.up);
             transform.rotation = Quaternion.Lerp(transform.rotation, newRot, _turnAroundSpeed);
+        }
+    }
+
+    private void TryToDamage(TestEnemy enemy)
+    {
+
+    }
+
+    private Vector3 AdjustVelocityToSlope(Vector3 velocity)
+    {
+        var ray = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(ray, out _slopeHit, 2f))
+        {
+            var slopeRotation = Quaternion.FromToRotation(Vector3.up, _slopeHit.normal);
+            var adjustedVelocity = slopeRotation * velocity;
+
+            if (adjustedVelocity.y < 0)
+            {
+                return adjustedVelocity;
+            }
+        }
+
+        return velocity;
+    }
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.CompareTag("Enemy") && _isSucking)
+        {
+            TryToDamage(collider.GetComponent<TestEnemy>());
         }
     }
 }
